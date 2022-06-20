e3cf3f18bd78e634448139d93cc4d44cae7a55f1
Dave Barnes <barnesy@juggler.net>
2022-06-20 09:53:43 +0200

    built npkg 1.0.1

--- Siteswap/Settings.cs
+++ Siteswap/Settings.cs
@@ -1,10 +1,13 @@
 ﻿namespace SiteswapLib
 {
+
+    // Need some IoC here so each project can handle its own settings
     public partial class Siteswap
     {
         public static bool AutoSort = false;
         public static bool BiggerAnimator = false;
         public static ColourMode ColourMode = ColourMode.orbit;
         public static bool Slomo = false;
+        public static bool ChartMode = false;
     }
 }
